521d60714fa2ee5f15485e47363a71c524f2e277
Alan Ho <alanho@gmail.com>
2015-05-05 16:54:37 +0800

    add infinite scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,34 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+$(function(){
+  $("a[data-source]").on("click", function(e){
+    _gaq.push(['_trackPageview', "/"+$(this).data("source")]);
+  })
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      $("#covers-container .pager-loader").click();
+    }
+  });
+
+  $(".cover a").tooltip();
+
+  $("#covers-container").on("click", ".pager-loader", function(e){
+    e.preventDefault();
+    var that = $(this);
+    var url = that.attr("href")
+    that.remove();
+
+    $.get(url).done(function(data){
+      $("#covers-container").append(data);
+    })
+  })
+
+  var konami = new Konami();
+  konami.code = function() {
+    
+  }
+  konami.load();
+})
